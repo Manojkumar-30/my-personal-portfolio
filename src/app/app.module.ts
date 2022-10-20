29b365af3d438c1858317f3e452d6553811b2e69
Manoj Kumar <94695167+Mkvelan@users.noreply.github.com>
2022-10-20 16:38:36 +0530

    initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing/landing.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { ServicesPageComponent } from './services-page/services-page.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingComponent,
+    TestimonyComponent,
+    ServicesPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
